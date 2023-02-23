fc757523e8bc323ed08ea78f333cf1c87fc20c1d
Florian.Schrittwiese <florian.schrittwieser@web.de>
2023-02-23 20:15:10 +0100

    Kommentare

--- stepper arduino/stepper arduino.ino
+++ stepper arduino/stepper arduino.ino
@@ -1,6 +1,6 @@
 const int DIR = 2;
 const int STEP = 3;
-const int steps_per_rev = 2000;
+const int steps_per_rev = 2000; //TODO nutze besser define die kannst du dann quer durch 
 
 const int DIR2 = 4;    // Stepper 2 
 const int STEP2 = 5;
@@ -13,10 +13,11 @@ void setup()
   pinMode(DIR, OUTPUT);
 
     pinMode(STEP2, OUTPUT);   // Neu Stepper 2 
-    pinMode(DIR2, OUTPUT);
+  //TODO sieht aus wie zusammengespaxt 
+    pinMode(DIR2, OUTPUT);    //TODO konstanter Abstand sieht bessser und professsioner aus
  
 }
-
+  //TODO baue/oder teste Code bevor du pisht, immer nur funktioniernden COde pushen
 void loop()
 {
   Serial.println("Spinning Clockwise...");
@@ -25,8 +26,14 @@ void loop()
 
 
 
-  do{
+  do{ //TODO nicht funcktional, da Durchführung nicht definiert ist
     for(int i = 0; i<steps_per_rev; i++)
+
+      //TODO Klammern fehlen
+      //for(...)
+      //{
+      //   Code   
+      //}
   
     digitalWrite(STEP, HIGH);
     delayMicroseconds(2000);
@@ -34,7 +41,8 @@ void loop()
     delayMicroseconds(2000);
   }while(Durchführugn)
 
-  do{
+  do{//TODO schiebe Code von in eine For schleife 
+  //-> einzelne SChritte sollen gleichzeitig und nicht nacheinander ausgeführt werden 
     for(int i = 0; i<steps_per_rev; i++)
   
     digitalWrite(STEP2, HIGH);
@@ -44,7 +52,7 @@ void loop()
   }while(Durchführung)
 
  
-                                                            
+    //TODO lösche unnötige KOmmentare, nie "toten Code"  pushen                               
                                                                // weis nicht ob ich die nachfolgenden noch Brauch 
                                                               // delay(1000); 
   
