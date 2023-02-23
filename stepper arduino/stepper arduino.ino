395550642fa5575a143761ad5d48dae1da1da1be
Florian.Schrittwiese <florian.schrittwieser@web.de>
2023-02-23 20:05:49 +0100

    Vorschlag: define, remove do while, refactoring

--- stepper arduino/stepper arduino.ino
+++ stepper arduino/stepper arduino.ino
@@ -1,10 +1,16 @@
+#define STEPS_PER_REVOLUTION 2000
+#define DELAY_STEPPER 2000;
+
 const int DIR = 2;
 const int STEP = 3;
-const int steps_per_rev = 2000;
+
+const int steps_per_rev = STEPS_PER_REVOLUTION;
 
 const int DIR2 = 4;    // Stepper 2 
 const int STEP2 = 5;
-const int steps_per_rev2 = 2000;
+const int steps_per_rev2 = STEPS_PER_REVOLUTION;
+
+int rev = 5;
 
 void setup()
 {
@@ -12,42 +18,29 @@ void setup()
   pinMode(STEP, OUTPUT);
   pinMode(DIR, OUTPUT);
 
-    pinMode(STEP2, OUTPUT);   // Neu Stepper 2 
-    pinMode(DIR2, OUTPUT);
- 
+  pinMode(STEP2, OUTPUT);   // Neu Stepper 2 
+  pinMode(DIR2, OUTPUT);
+  
+  //switch on Stepper driver at System initialisation
+  digitalWrite(DIR, HIGH);
+  digitalWrite(DIR2, HIGH); // Neu Stepper 2 
 }
 
 void loop()
-{
+{ 
   Serial.println("Spinning Clockwise...");
-  digitalWrite(DIR, HIGH);
-  digitalWrite(DIR2, HIGH); // Neu Stepper 2 
-
-
-
-  do{
-    for(int i = 0; i<steps_per_rev; i++)
   
+  //define stepper motor revolutions
+  rev = 1;
+
+  //turn both motor simustanosly until target revolutions are done
+  for(int i = 0; i < rev * STEPS_PER_REVOLUTION; i++)
+  {
     digitalWrite(STEP, HIGH);
-    delayMicroseconds(2000);
-    digitalWrite(STEP, LOW);
-    delayMicroseconds(2000);
-  }while(Durchführugn)
-
-  do{
-    for(int i = 0; i<steps_per_rev; i++)
-  
     digitalWrite(STEP2, HIGH);
-    delayMicroseconds(2000);
+    delayMicroseconds(DELAY_STEPPER);
+    digitalWrite(STEP, LOW);
     digitalWrite(STEP2, LOW);
-    delayMicroseconds(2000);
-  }while(Durchführung)
-
- 
-                                                            
-                                                               // weis nicht ob ich die nachfolgenden noch Brauch 
-                                                              // delay(1000); 
-  
-                                                             //digitalWrite(DIR, LOW);
-                                                             //Serial.println("Spinning Anti-Clockwise...");
+    delayMicroseconds(DELAY_STEPPER);
+  }
 }
