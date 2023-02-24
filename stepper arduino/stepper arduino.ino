e065cc456a20adad113bb9c9f8148586ac5482ef
Nolixle <lenz_noah@t-online.de>
2023-02-24 18:29:27 +0100

    Doku Ablauf

--- stepper arduino/stepper arduino.ino
+++ stepper arduino/stepper arduino.ino
@@ -11,51 +11,30 @@ void setup()
   Serial.begin(115200);
   pinMode(STEP, OUTPUT);
   pinMode(DIR, OUTPUT);
-
-    pinMode(STEP2, OUTPUT);   // Neu Stepper 2 
-  //TODO sieht aus wie zusammengespaxt 
-    pinMode(DIR2, OUTPUT);    //TODO konstanter Abstand sieht bessser und professsioner aus
- 
+  pinMode(STEP2, OUTPUT);   // Neu Stepper 2 
+  pinMode(DIR2, OUTPUT);    
 }
-  //TODO baue/oder teste Code bevor du pisht, immer nur funktioniernden COde pushen
+  //TODO baue/oder teste Code bevor du pusht, immer nur funktioniernden COde pushen
 void loop()
 {
   Serial.println("Spinning Clockwise...");
   digitalWrite(DIR, HIGH);
-  digitalWrite(DIR2, HIGH); // Neu Stepper 2 
-
-
-
-  do{ //TODO nicht funcktional, da Durchführung nicht definiert ist
+  digitalWrite(DIR2, HIGH); // Neu Stepp
+  
+   
     for(int i = 0; i<steps_per_rev; i++)
 
-      //TODO Klammern fehlen
-      //for(...)
-      //{
-      //   Code   
-      //}
-  
     digitalWrite(STEP, HIGH);
     delayMicroseconds(2000);
     digitalWrite(STEP, LOW);
     delayMicroseconds(2000);
-  }while(Durchführugn)
-
-  do{//TODO schiebe Code von in eine For schleife 
-  //-> einzelne SChritte sollen gleichzeitig und nicht nacheinander ausgeführt werden 
+ 
     for(int i = 0; i<steps_per_rev; i++)
   
     digitalWrite(STEP2, HIGH);
     delayMicroseconds(2000);
     digitalWrite(STEP2, LOW);
     delayMicroseconds(2000);
-  }while(Durchführung)
 
- 
-    //TODO lösche unnötige KOmmentare, nie "toten Code"  pushen                               
-                                                               // weis nicht ob ich die nachfolgenden noch Brauch 
-                                                              // delay(1000); 
   
-                                                             //digitalWrite(DIR, LOW);
-                                                             //Serial.println("Spinning Anti-Clockwise...");
 }
