0ed9fa55772742650d4095fce86d7d5d1ddc9b6b
Nolixle <lenz_noah@t-online.de>
2023-03-01 16:05:02 +0100

    New Idea

--- stepper arduino/stepper arduino.ino
+++ stepper arduino/stepper arduino.ino
@@ -1,41 +1,17 @@
-const int DIR = 2;
-const int STEP = 3;
-const int steps_per_rev = 2000; //TODO nutze besser define die kannst du dann quer durch 
+#include <Stepper.h>
+int SPU = 2048; 
+Stepper Motor(SPU, 3,5,4,6);
 
-const int DIR2 = 4;    // Stepper 2 
-const int STEP2 = 5;
-const int steps_per_rev2 = 2000;
-
-void setup()
+void setup() 
 {
-  Serial.begin(115200);
-  pinMode(STEP, OUTPUT);
-  pinMode(DIR, OUTPUT);
-  pinMode(STEP2, OUTPUT);   // Neu Stepper 2 
-  pinMode(DIR2, OUTPUT);    
+Motor.setSpeed(5);
 }
-  //TODO baue/oder teste Code bevor du pusht, immer nur funktioniernden COde pushen
-void loop()
-{
-  Serial.println("Spinning Clockwise...");
-  digitalWrite(DIR, HIGH);
-  digitalWrite(DIR2, HIGH); // Neu Stepp
-  
-   
-    for(int i = 0; i<steps_per_rev; i++)
 
-    digitalWrite(STEP, HIGH);   // da wird ein Schritt gemacht 
-    delayMicroseconds(2000);
-    digitalWrite(STEP, LOW);
-    delayMicroseconds(2000);
- 
-    for(int i = 0; i<steps_per_rev; i++)
-  
-    digitalWrite(STEP2, HIGH);
-    delayMicroseconds(2000);
-    digitalWrite(STEP2, LOW);
-    delayMicroseconds(2000);
-
-  
+void loop() {
+Motor.step(2048);
+delay(1000); 
+Motor.step(-2048); 
+delay(1000);
 }
+
 
