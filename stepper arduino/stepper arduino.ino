703f6d4c0d7bbba470f71e43ffe99643c7923fac
Florian.Schrittwiese <florian.schrittwieser@web.de>
2023-03-11 07:34:40 +0100

    proposal how a programm could look likenot tested

--- stepper arduino/stepper arduino.ino
+++ stepper arduino/stepper arduino.ino
@@ -1,35 +1,99 @@
 #define STEPS_PER_REVOLUTION 2000
 #define DELAY_STEPPER 2000;
 
-const int DIR = 2;
-const int STEP = 3;
+void BeerPouringRoutine(void);
+void StartSwitchISR(void);
+void EmergencyStopISR(void);
+void ErrorState(void);
+void EmergencyStopPressed(void);
 
-const int steps_per_rev = STEPS_PER_REVOLUTION;
+//TODO Not aus as interrup, switch off stepper
 
-const int DIR2 = 4;    // Stepper 2 
-const int STEP2 = 5;
-const int steps_per_rev2 = STEPS_PER_REVOLUTION;
+const int DIR = 4;
+const int STEP = 5;
+
+const int DIR2 = 6;    // Stepper 2 
+const int STEP2 = 7;
+
+
+const int EmergencySwitch = 2; //real interrupt pin 
+bool EmergencySwitchStatus = TRUE;
+const int StartSwitch = 3; //real interrupt pin 
+bool StartSwitchStatus = FALSE;
 
 int rev = 5;
 
 void setup()
 {
   Serial.begin(115200);
+  Serial.println("Startet Biereinschenkautomat");
+  
+  pinMode(EmergencySwitch, INPUT);
+  pinMode(EmergencySwitch, HIGH);
+  pinMode(StartSwitch, INPUT);
+  pinMode(StartSwitch, LOW);
+  //Interrupt  for Emergency switch and StartSwitch 
+  attachInterrupt(digitalPintInterrupt(EmergencySwitch), EmergencyStopISR, FALLING);
+  attachInterrupt(digitalPintInterrupt(StartSwitchStatus), StartSwitchISR, RISING);
+  Serial.println("Interrupt initialized");
+
   pinMode(STEP, OUTPUT);
   pinMode(DIR, OUTPUT);
 
-  pinMode(STEP2, OUTPUT);   // Neu Stepper 2 
-  pinMode(DIR2, OUTPUT);
-  
-  //switch on Stepper driver at System initialisation
-  digitalWrite(DIR, HIGH);
-  digitalWrite(DIR2, HIGH); // Neu Stepper 2 
+  pinMode(STEP2, OUTPUT);   
+  Serial.println("Pins configured");
+
+  if(digitalRead(EmergencySwitch) != HIGH)
+  {
+    EmergencySwitchStatus = TRUE;
+  }
+  else{
+    EmergencySwitchStatus = FALSE;
+  }
+  Serial.println("Emergency Stop is pressed %c, [1 pressed, 0 not pressed]", EmergencySwitchStatus);
+  Serial.println("Initializing done");
+  Serial.println("-------------------------------------------");
 }
 
 void loop()
 { 
-  Serial.println("Spinning Clockwise...");
-  
+  //run BeerPouringroutine when start button is pressed and Emergency Switch is not pressed 
+  if(FALSE != EmergencySwitch && TRUE == StartSwitch)
+  {
+    BeerPouringRoutine();
+  }
+  else if(FALSE == StartSwitch) //Status messages 
+  {
+    Serial.println("Start Switch not pressed");
+  }
+   else if(TRUE == EmergencySwitch)
+  {
+    Serial.println("Emergency Switch pressed");
+  }
+}
+
+void EmergencyStopISR(void)
+{
+  //Switch off Stepper
+  digitalWrite(STEP, LOW);
+  digitalWrite(STEP2, LOW);
+  EmergencySwitchStatus = TRUE;
+}
+
+void StartSwitchISR(void)
+{
+  //only if System is not Busy start new iteration of Beer
+  digitalWrite(STEP, LOW);
+  digitalWrite(STEP2, LOW);
+  StartSwitchStatus = TRUE;
+}
+
+void BeerPouringRoutine(void)
+{
+  //define spinning direction of stepper
+  digitalWrite(DIR, HIGH);
+  digitalWrite(DIR2, HIGH); // Neu Stepper 2 
+      
   //define stepper motor revolutions
   rev = 1;
 
