f983afe20939801e94c7ad199544530b2c5778b1
Nolixle <lenz_noah@t-online.de>
2023-03-22 21:54:40 +0100

    improved

--- stepper arduino/stepper arduino.ino
+++ stepper arduino/stepper arduino.ino
@@ -16,34 +16,52 @@ void funktion()
                                  
   for(int i = 0; i<STEPS_PER_REV; i++) 
   { 
-    digitalWrite(STEP, HIGH);   // da wird ein Schritt gemacht 
-    delayMicroseconds(2000);
+    digitalWrite(STEP, HIGH);
+    digitalWrite(STEP2, HIGH);   // da wird ein Schritt gemacht 
+    delayMicroseconds(1000);
     digitalWrite(STEP, LOW);
-    delayMicroseconds(2000);
-  } 
-  for(int i = 0; i<STEPS_PER_REV; i++) 
-  {
-    digitalWrite(STEP2, HIGH);
-    delayMicroseconds(2000);
     digitalWrite(STEP2, LOW);
-    delayMicroseconds(2000);
-  }
-}
+    delayMicroseconds(1000);
+  } 
+for(int i = 0; i<STEPS_PER_REV; i++) 
+ {
+   digitalWrite(STEP, HIGH);
+   delayMicroseconds(1000);
+   digitalWrite(STEP, LOW);
+    delayMicroseconds(1000);
+    }
+   
+   
 
+
+}
+void funktion2()
+{
+  for(int i = 0; i<STEPS_PER_REV; i++) 
+     {
+        digitalWrite(STEP2, LOW);
+    delayMicroseconds(1000);
+    digitalWrite(STEP2, HIGH);   // da wird ein Schritt gemacht 
+    delayMicroseconds(1000);
+     }
+}
 void setup()
 {
   Serial.begin(115200);
   pinMode(STEP, OUTPUT);
   pinMode(DIR, OUTPUT);
   pinMode(STEP2, OUTPUT);   
-  pinMode(DIR2, OUTPUT);    
+  pinMode(DIR2, OUTPUT);
+ 
 }
 
 void loop()
-{
+
+{  
   Serial.println("Spinning Clockwise...");
   digitalWrite(DIR, HIGH);
   digitalWrite(DIR2, HIGH); // Neu Stepp
+  
   funktion(); 
 }
 
