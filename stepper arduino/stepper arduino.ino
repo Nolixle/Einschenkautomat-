d0607619a0ffa08ec481c7b545cfee601ebe5851
Nolixle <lenz_noah@t-online.de>
2023-03-22 18:23:35 +0100

    Partly controlled

--- stepper arduino/stepper arduino.ino
+++ stepper arduino/stepper arduino.ino
@@ -1,25 +1,25 @@
 #define DIR  2
 #define STEP  3
-#define steps_per_rev  2000 //STEPS_PER_REV //TODO define are always written with CAPITAL_LETTERS -> this is due to better readability
+#define STEPS_PER_REV  2000 
 //when you built the code the compiler will replace all the names with the number it was defined
 //when you write it as a normal variable you will be confused, which one is normal variable and the value can be changed or not 
 
 #define DIR2  4    // Stepper 2 
 #define STEP2  5
-#define steps_per_rev2  2000
+#define STEPS_PER_REV2  2000
 
- void Funkton1(); //Functions never used 
- void Funktin2() //; //TODO Semicolon missing and typo Function is the correct spelling 
+ void funktion1(); //Functions never used 
+ void funktion2(); 
 {
                                  
-  for(int i = 0; i<steps_per_rev; i++)  //STEPS_PER_REV
+  for(int i = 0; i<STEPS_PER_REV; i++) 
   //{ //TODO brakets missing, you will get a compilier warning 
     digitalWrite(STEP, HIGH);   // da wird ein Schritt gemacht 
     delayMicroseconds(2000);
     digitalWrite(STEP, LOW);
     delayMicroseconds(2000);
   //} //TODO braket missing 
-    for(int i = 0; i<steps_per_rev; i++) //STEPS_PER_REV
+    for(int i = 0; i<STEPS_PER_REV; i++) 
   //{ TODO breaket missing
     digitalWrite(STEP2, HIGH);
     delayMicroseconds(2000);
@@ -51,14 +51,14 @@ void loop()
   digitalWrite(DIR2, HIGH); // Neu Stepp
   
    
-    for(int i = 0; i<steps_per_rev; i++)
+    for(int i = 0; i<STEPS_PER_REV; i++)
 
     digitalWrite(STEP, HIGH);   // da wird ein Schritt gemacht 
     delayMicroseconds(2000);
     digitalWrite(STEP, LOW);
     delayMicroseconds(2000);
    
-    for(int i = 0; i<steps_per_rev; i++)
+    for(int i = 0; i<STEPS_PER_REV; i++)
   
     digitalWrite(STEP2, HIGH);
     delayMicroseconds(2000);
@@ -68,7 +68,7 @@ void loop()
 
 
 }
-void Funktion1()
+void funktion1()
 {
 
 }
