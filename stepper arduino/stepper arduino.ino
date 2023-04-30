384dde22bf2f515ce3790fd488c73ae5127d5969
Nolixle <lenz_noah@t-online.de>
2023-04-30 17:46:17 +0200

    Interupt funktioniert nicht

--- stepper arduino/stepper arduino.ino
+++ stepper arduino/stepper arduino.ino
@@ -1,5 +1,5 @@
-#define DIR  2
-#define STEP  3
+#define DIR  6
+#define STEP  7
 #define STEPS_PER_REV  2000 
 
 #define DIR2  4    // Stepper 2 
@@ -7,8 +7,12 @@
 #define STEPS_PER_REV2  2000
 
 void BeerPouringRoutine(void);
-void StartSwitchISR(void);
-void EmergencyStopISR(void);
+// void StartSwitchISR(void);
+void StartSwitchON(void);
+void StartSwitchOFF(void);
+// void EmergencyStopISR(void);
+void EmergencyStopON(void);
+void EmergencyStopOFF(void);
 void ErrorState(void);
 void EmergencyStopPressed(void);
 void Ranfuehren(void);  
@@ -17,9 +21,9 @@ void Hefeextrahieren(void);
 void Hefeeinschenken(void);
 void Fertigstellen(void);
 
-const int EmergencySwitch = 6; //real interrupt pin 
+const int EmergencySwitch = 2; //real interrupt pin 
 bool EmergencySwitchStatus = true;
-const int StartSwitch = 7; //real interrupt pin 
+const int StartSwitch = 3; //real interrupt pin 
 bool StartSwitchStatus = false;
 
 int rev = 5;
@@ -31,7 +35,7 @@ void setup()
   Serial.println("Startet Biereinschenkautomat");
   
   pinMode(EmergencySwitch, INPUT);
-  pinMode(EmergencySwitch, HIGH);
+  pinMode(EmergencySwitch, LOW);
   pinMode(StartSwitch, INPUT);
   pinMode(StartSwitch, LOW);
   pinMode(STEP, OUTPUT);
@@ -39,8 +43,8 @@ void setup()
   pinMode(STEP2, OUTPUT);   
   pinMode(DIR2, OUTPUT);
 
-  attachInterrupt(digitalPinToInterrupt(EmergencySwitch), EmergencyStopISR, FALLING);
-  attachInterrupt(digitalPinToInterrupt(StartSwitchStatus), StartSwitchISR, RISING);
+  //attachInterrupt(digitalPinToInterrupt(EmergencySwitch), EmergencyStopISR, FALLING);
+  //attachInterrupt(digitalPinToInterrupt(StartSwitch), StartSwitchISR, RISING);
   
   Serial.println("Pins configured");
 
@@ -54,6 +58,16 @@ void setup()
     Serial.println("Emergency Stop is NOT pressed");
   }
 
+  if(digitalRead(StartSwitch) != HIGH)
+  {
+    StartSwitchStatus = false;
+    Serial.println("Start is PRESSED");
+  }
+  else{
+    StartSwitchStatus = true;
+    Serial.println("Start is NOT pressed");
+  }
+
   Serial.println("Initializing done");
   Serial.println("-------------------------------------------");
 }
@@ -61,35 +75,79 @@ void setup()
 
 void loop()
 { 
+
   //run BeerPouringroutine when start button is pressed and Emergency Switch is not pressed 
-  if(false != EmergencySwitch && true == StartSwitch)
+  // Serial.print("start: ");
+  // Serial.println(digitalRead(2));
+  // Serial.print("stop: ");
+  // Serial.println(digitalRead(3));
+
+  if(HIGH == digitalRead(StartSwitch))
+  {
+    StartSwitchON();
+  }
+  else
+  {
+    StartSwitchOFF();
+  }
+  if(HIGH == digitalRead(EmergencySwitch))
+  {
+    EmergencyStopON();
+  }
+  else
+  {
+    EmergencyStopOFF();
+  }
+
+  if(true != EmergencySwitchStatus && true == StartSwitchStatus)
   {
     BeerPouringRoutine();
   }
-  else if(false == StartSwitch) //Status messages 
+  else if(false == StartSwitchStatus) //Status messages 
   {
     Serial.println("Start Switch not pressed");
   }
-   else if(true == EmergencySwitch)
+   else if(true == EmergencySwitchStatus)
   {
     Serial.println("Emergency Switch pressed");
   }
 }
 
-void EmergencyStopISR(void)
+void EmergencyStopON(void)
 {
   //Switch off Stepper
   digitalWrite(STEP, LOW);
   digitalWrite(STEP2, LOW);
   EmergencySwitchStatus = true;
+  // Serial.println("Stop ON");
 }
 
-void StartSwitchISR(void)
+void EmergencyStopOFF(void)
+{
+  //Switch off Stepper
+  digitalWrite(STEP, HIGH);
+  digitalWrite(STEP2, HIGH);
+  EmergencySwitchStatus = false;
+  // Serial.println("Stop OFF");
+}
+
+
+void StartSwitchON(void)
+{
+  //only if System is not Busy start new iteration of Beer
+  digitalWrite(STEP, HIGH);
+  digitalWrite(STEP2, HIGH);
+  StartSwitchStatus = true;
+  // Serial.println("Start ON");
+}
+
+void StartSwitchOFF(void)
 {
   //only if System is not Busy start new iteration of Beer
   digitalWrite(STEP, LOW);
   digitalWrite(STEP2, LOW);
-  StartSwitchStatus = true;
+  StartSwitchStatus = false;
+  // Serial.println("Start OFF");
 }
 
 void BeerPouringRoutine(void)
@@ -120,7 +178,7 @@ void BeerPouringRoutine(void)
   Fertigstellen();
 }
 
-void Ranfueren()
+void Ranfuehren(void)
 {
   for(int i = 0; i<STEPS_PER_REV; i++) 
   { 
@@ -134,14 +192,14 @@ void Ranfueren()
   } 
 }
 
-void Einschenken()
+void Einschenken(void)
 {
   for(int i = 0; i<STEPS_PER_REV; i++) 
   {
     digitalWrite(STEP, HIGH);
-    delayMicroseconds(1000);
+    delayMicroseconds(1500);
     digitalWrite(STEP, LOW); 
-    delayMicroseconds(1000);
+    delayMicroseconds(1500);
     digitalWrite(STEP2, HIGH);
     delayMicroseconds(1000);
     digitalWrite(STEP2, LOW);  
@@ -149,7 +207,7 @@ void Einschenken()
   }
 }
 
-void Hefeextrahieren()
+void Hefeextrahieren(void)
 {
   for(int i = 0; i<STEPS_PER_REV; i++) 
   {
@@ -160,7 +218,7 @@ void Hefeextrahieren()
   }
 }
 
-void Hefeeinschenken()
+void Hefeeinschenken(void)
 {
   for(int i = 0; i<STEPS_PER_REV; i++) 
   {   
@@ -171,7 +229,7 @@ void Hefeeinschenken()
   }
 }
 
-void Fertigstellen()
+void Fertigstellen(void)
 {
   for(int i = 0; i<STEPS_PER_REV; i++) 
   {   
