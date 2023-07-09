da020c723fb1d2d0f619ac346b6f31e873ad5a19
Florian.Schrittwiese <florian.schrittwieser@web.de>
2023-07-09 16:26:57 +0200

    statemachine

--- stepper arduino/stepper arduino.ino
+++ stepper arduino/stepper arduino.ino
@@ -1,3 +1,10 @@
+/*
+* Project: Biereinschenkautomat 
+* Date: 08.05.2023
+* Hardware: Arduino Uno
+*/
+
+// preprocessor 
 #define DIR  6
 #define STEP  7
 #define STEPS_PER_REV  2000 
@@ -5,24 +12,50 @@
 #define DIR2  4    // Stepper 2 
 #define STEP2  5
 #define STEPS_PER_REV2  2000
+//Debug macro, Enabled for checking the functionality of Switches 
+#define DEBUG
 
-// #define DEBUG
+// datatypes
+typedef enum{
+  UNINITIALIZED = 0,
+  INITIALIZING,
+  IDLE,
+  ACTIVE,
+  ERROR
+}SMState_Type;
 
+//Function declaration
 void BeerPouringRoutine(void);
-void StartSwitchISR(void);
-void EmergencyStopISR(void);
+void StartSwitchON(void);
+void StartSwitchOFF(void);
+void EmergencyStopON(void);
+void EmergencyStopOFF(void);
 void Ranfuehren(void);  
 void Einschenken(void);
 void Hefeextrahieren(void);
 void Hefeeinschenken(void);
 void Fertigstellen(void);
+void movetoStart(void);
 
-const int EmergencySwitch = 2; //real interrupt pin 
-bool EmergencySwitchStatus = true;
-const int StartSwitch = 3; //real interrupt pin 
-bool StartSwitchStatus = false;
+//Pin declaration
+const int EmergencySwitch = 2; // interrupt pin 
+const int StartSwitch = 3; // interrupt pin 
+const int EndSwitchStep1Start = 10;
+const int EndSwitchStep1End = 11;
+const int EndSwitchStep2Start = 12;
+const int EndSwitchStep2End = 13;
 
+//variable declaration
+SMState_Type SMState = UNINITIALIZED;
+volatile bool EmergencySwitchStatus = true;
+volatile bool StartSwitchStatus = false;
+volatile bool isReferencedStep1 = false;
+volatile bool isReferencedStep2 = false;
 int rev = 5;
+
+int i = 0;
+int d = 0;
+int b = 0;
 
 void setup()
 {
@@ -30,16 +63,18 @@ void setup()
   Serial.println("Startet Biereinschenkautomat");
   
   pinMode(EmergencySwitch, INPUT);
-  pinMode(EmergencySwitch, LOW);
+  pinMode(EmergencySwitch, HIGH);
   pinMode(StartSwitch, INPUT);
-  pinMode(StartSwitch, LOW);
+  pinMode(StartSwitch, HIGH);
   pinMode(STEP, OUTPUT);
   pinMode(DIR, OUTPUT);
-  pinMode(STEP2, OUTPUT);   
+  pinMode(STEP2, OUTPUT);
   pinMode(DIR2, OUTPUT);
 
-  attachInterrupt(digitalPinToInterrupt(EmergencySwitch), EmergencyStopISR, FALLING);
-  attachInterrupt(digitalPinToInterrupt(StartSwitch), StartSwitchISR, RISING);
+  // attachInterrupt(digitalPinToInterrupt(EmergencySwitch), EmergencyStopON, RISING);
+  attachInterrupt(digitalPinToInterrupt(EmergencySwitch), EmergencyStopOFF, FALLING);
+  attachInterrupt(digitalPinToInterrupt(StartSwitch), StartSwitchON, RISING);
+  attachInterrupt(digitalPinToInterrupt(StartSwitch), StartSwitchOFF, FALLING);
   
   Serial.println("Pins configured");
 
@@ -65,76 +100,114 @@ void setup()
     Serial.println("Start is NOT pressed");
   }
 
-  Serial.println("Initializing done");
+  Serial.println("Setup done");
   Serial.println("-------------------------------------------");
 }
-
 
 void loop()
 { 
 #ifdef DEBUG
   // run BeerPouringroutine when start button is pressed and Emergency Switch is not pressed 
-  Serial.print("start: ");
-  Serial.println(digitalRead(StartSwitch));
-  Serial.print("stop: ");
-  Serial.println(digitalRead(EmergencySwitch));
+  // Serial.print("start: ");
+  // Serial.print(digitalRead(StartSwitch));
+  // Serial.print(", start Status: ");
+  // // Serial.print(",");
+  // Serial.print(StartSwitchStatus);
+  // // Serial.print(",");
+  // Serial.print(", EM Stop: ");
+  // Serial.print(digitalRead(EmergencySwitch));
+  // // Serial.print(",");
+  // Serial.print(", EM Status: ");
+  // Serial.println(EmergencySwitchStatus);
 #endif
+  Statemachine();
+}
 
-  if(true != EmergencySwitchStatus && true == StartSwitchStatus)
+void Statemachine(void)
+{
+  switch (SMState)
   {
-    BeerPouringRoutine();
-  }
-  else if(false == StartSwitchStatus) //Status messages 
-  {
-    Serial.println("Start Switch not pressed");
-  }
-   else if(true == EmergencySwitchStatus)
-  {
-    Serial.println("Emergency Switch pressed");
+  case UNINITIALIZED:
+    SM_Uninitialized();
+    break;
+  case INITIALIZING:
+    SM_Initializing();
+    break;
+  case IDLE:
+    SM_Idle();
+    break;
+  case ACTIVE:
+    SM_Active();
+    break;
+  case ERROR:
+    SM_Error();
+    break;
+  default:
+    SMState = ERROR;
+    break;
   }
 }
 
-void EmergencyStopISR(void)
+void EmergencyStopOFF(void)
 {
   //Switch off Stepper
-  digitalWrite(STEP, LOW);
-  digitalWrite(STEP2, LOW);
-  EmergencySwitchStatus = true;
-  Serial.println("EMStop ON");
+  digitalWrite(STEP, HIGH);
+  digitalWrite(STEP2, HIGH);
+  EmergencySwitchStatus = false;
+  // delay(500);
+  Serial.println("EM Stop OFF");
+  i++;
+  Serial.print("i:");
+  Serial.println(i);
 }
 
-void StartSwitchISR(void)
+void StartSwitchON(void)
 {
-  //only if System is not Busy start new iteration of Beer
   digitalWrite(STEP, HIGH);
   digitalWrite(STEP2, HIGH);
   StartSwitchStatus = true;
+  // delay(500);
   Serial.println("Start ON");
+  b++;
+  Serial.print("b:");
+  Serial.println(b);
+}
+
+void StartSwitchOFF(void)
+{
+  digitalWrite(STEP, LOW);
+  digitalWrite(STEP2, LOW);
+  StartSwitchStatus = false;
+  // delay(500);
+  Serial.println("Start OFF");
+  d++;
+  Serial.print("d:");
+  Serial.println(d);
 }
 
 void BeerPouringRoutine(void)
 {  
-  Serial.println("Spinning Clockwise...");
+  // Serial.println("Spinning Clockwise...");
   digitalWrite(DIR, HIGH);
   digitalWrite(DIR2, HIGH);
   Ranfuehren(); 
 
-  Serial.print("Spinning Clockwise...");
+  // Serial.print("Spinning Clockwise...");
   digitalWrite(DIR, LOW);
   digitalWrite(DIR2, HIGH); 
   Einschenken();
 
-  Serial.print("Spinning Clockwise...");
+  // Serial.print("Spinning Clockwise...");
   digitalWrite(DIR, LOW);
   digitalWrite(DIR2, LOW); 
   Hefeextrahieren();
 
-  Serial.print("Spinning Clockwise...");
+  // Serial.print("Spinning Clockwise...");
   digitalWrite(DIR, LOW);
   digitalWrite(DIR2, HIGH); 
   Hefeeinschenken();
 
-  Serial.print("Spinning Clockwise...");
+  // Serial.print("Spinning Clockwise...");
   digitalWrite(DIR, LOW);
   digitalWrite(DIR2, LOW); 
   Fertigstellen();
@@ -201,4 +274,75 @@ void Fertigstellen(void)
     digitalWrite(STEP2, LOW);
     delayMicroseconds(1000);
   }  
+}
+
+void movetoStart(void)
+{
+  if(!isReferencedStep1 || !isReferencedStep2)
+  {
+    digitalWrite(DIR, HIGH);
+    digitalWrite(DIR2, HIGH);
+
+    if(LOW != digitalRead(EndSwitchStep1Start))
+    {
+      digitalWrite(STEP, HIGH);
+      delayMicroseconds(100);
+      digitalWrite(STEP, LOW);
+      delayMicroseconds(100);
+    }
+    else
+    {
+      isReferencedStep1 = true;
+    }
+    if(LOW != digitalRead(EndSwitchStep2Start))
+    {
+      digitalWrite(STEP2, HIGH);
+      delayMicroseconds(100);
+      digitalWrite(STEP2, LOW);
+      delayMicroseconds(100);
+    }
+    else
+    {
+      isReferencedStep2 = true;
+    }
+  }
+}
+
+void SM_Uninitialized(void)
+{
+  // Serial.println("SM: Uninitialized");
+  SMState = INITIALIZING;
+}
+
+void SM_Initializing(void)
+{
+  // Serial.println("SM: Initializing");
+  if(!isReferencedStep1 || !isReferencedStep2)
+  {
+    movetoStart();
+  }
+  else
+  {
+    SMState = IDLE;
+  }
+}
+
+void SM_Idle(void)
+{
+  // Serial.println("SM: Idle");
+  if(StartSwitchStatus)
+  {
+    SMState = ACTIVE;
+  } 
+}
+
+void SM_Active(void)
+{
+  // Serial.println("SM: Active");
+  BeerPouringRoutine();
+}
+
+void SM_Error(void)
+{
+  Serial.println("SM: Error");
 }
